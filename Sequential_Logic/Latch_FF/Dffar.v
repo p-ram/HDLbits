3a249e187418e3ab30b2f3175c352a43affc8e20
p-ram <94652181+p-ram@users.noreply.github.com>
2023-01-07 21:31:34 -0800

    Create Dffar.v

---
+++ Sequential_Logic/Latch_FF/Dffar.v
@@ -0,0 +1,16 @@
+//async reset, posedge clk, 8-DFF
+`default_nettype none
+module top_module (
+    input clk,
+    input areset,   // active high asynchronous reset
+    input [7:0] d,
+    output [7:0] q
+);
+    always@(posedge clk or posedge areset) begin
+        if(areset)
+            q<=0;
+        else
+            q<=d;
+    end
+
+endmodule
